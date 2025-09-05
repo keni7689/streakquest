8e3f4aeb0952e167e71cdd15cc21d0f52dfaafb9
keni7689 <yogeshshejul54@gmail.com>
2025-09-05 20:08:49 +0530

    commit added the logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/refresh.png" />
+      </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
         <Analytics />
